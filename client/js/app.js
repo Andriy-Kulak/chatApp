684d355bf91c4778f0406d1f403a065b2bd4c9a0
Andriy <andkulak@gmail.com>
2016-01-04 14:07:25 -0500

    Updated directory structure

--- client/js/app.js
+++ client/js/app.js
@@ -1,12 +1,14 @@
 //Module
 var myApp = angular.module('myApp', ['ngRoute', 'ui.bootstrap', 'btford.socket-io']);
 
-//Routes
+/**
+ * Routes
+ */
 myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
-	$routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'homeController'});
-	$routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'aboutController'});
-	$routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: 'contactController'});
-	$routeProvider.when('/chat', {templateUrl: 'partials/chat.html', controller: 'chatController'});
+	$routeProvider.when('/home', {templateUrl: 'sections/home.html', controller: 'homeController'});
+	$routeProvider.when('/about', {templateUrl: 'sections/about.html', controller: 'aboutController'});
+	$routeProvider.when('/contact', {templateUrl: 'sections/contact.html', controller: 'contactController'});
+	$routeProvider.when('/chat', {templateUrl: 'sections/chat.html', controller: 'chatController'});
 
 	//if no valid routes are found, redirect to home
 	$routeProvider.otherwise({redirectTo: '/home'});
